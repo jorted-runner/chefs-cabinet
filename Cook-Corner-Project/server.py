1ca55af027a71706980aa1d2c869a9840e254deb
jorted-runner <jorted.runner1996@gmail.com>
2024-02-12 19:33:30 -0600

    New Recipe Nav Bar

--- Cook-Corner-Project/server.py
+++ Cook-Corner-Project/server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/new-recipe")
+def new_recipe():
+    return render_template('new-recipe.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = 8080, debug=True)
