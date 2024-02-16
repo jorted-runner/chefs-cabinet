dfb684f2890a728fb6b63c3cfc4c605c5109c2f7
jorted-runner <jorted.python@gmail.com>
2024-02-16 13:20:35 -0600

    AI Form Update

--- Cook-Corner-Project/static/scripts/recipeType.js
+++ Cook-Corner-Project/static/scripts/recipeType.js
@@ -79,13 +79,90 @@ function aiRecipe() {
         alert("Cannot add a blank ingredient. Add an ingredient and try again.");
         inputElmnt.focus();
     }
-});
+  });
 
   formElmnt.appendChild(ingredients_H1);
   formElmnt.appendChild(labelElmnt);
   formElmnt.appendChild(inputElmnt);
   formElmnt.appendChild(buttonElmnt);
   formElmnt.appendChild(ingredientsUL);
+
+  const hrElmnt = document.createElement('hr');
+  formElmnt.appendChild(hrElmnt);
+
+  const ingredients_exclude_H1 = document.createElement('h1');
+  ingredients_exclude_H1.textContent = "Ingredients to Exclude";
+  
+  const labelElmnt_2 = document.createElement('label');
+  labelElmnt_2.setAttribute('for', 'newIngredient');
+  labelElmnt_2.textContent = 'New Ingredient: ';
+
+  const inputElmnt_2 = document.createElement('input');
+  inputElmnt_2.setAttribute('type', 'text');
+  inputElmnt_2.setAttribute('id', 'newIngredient');
+  inputElmnt_2.setAttribute('placeholder', 'Gluten');
+
+  const buttonElmnt_2 = document.createElement('button');
+  buttonElmnt_2.setAttribute('type', 'submit');
+  buttonElmnt_2.textContent = 'Add';
+
+  const ingredientsUL_2 = document.createElement('ul');
+  ingredientsUL_2.setAttribute('id', 'ingredientsExclude');
+
+  buttonElmnt_2.addEventListener('click', () => {
+    if (inputElmnt_2.value != '') {
+      const ingredient = document.createElement('li');
+      const deleteButton = document.createElement('button');
+      ingredient.textContent = inputElmnt_2.value;
+      ingredientsExclude.push(inputElmnt_2.value);
+      deleteButton.textContent = "❌";
+      ingredient.append(deleteButton);
+      ingredientsUL_2.appendChild(ingredient);
+      deleteButton.addEventListener("click", () => {
+        ingredientsUL_2.removeChild(ingredient);
+        // I need to work on removing the element from the list
+        ingredientsExclude = ingredientsExclude.filter(item => item != ingredient.textContent);
+        console.log(ingredientsExclude);
+      });
+      inputElmnt_2.value = '';
+      inputElmnt_2.focus();
+      console.log(ingredientsExclude);
+    } else {
+      alert("Cannot add a blank ingredient. Add an ingredient and try again.");
+      inputElmnt_2.focus();
+    }
+  });
+  // I also need to refactor this function. It shouldn't have different names for elements than the one above.
+  document.addEventListener('keypress', function(event) {
+    let keyPressed = event.key;
+    if (keyPressed == "Enter" && inputElmnt_2.value != "") {
+        const newIngredient = document.createElement('li');
+        const deleteButton = document.createElement('button')
+        newIngredient.textContent = inputElmnt_2.value;
+        ingredientsInclude.push(inputElmnt_2.value);
+        deleteButton.textContent = "❌";
+        newIngredient.append(deleteButton);
+        ingredientsUL_2.appendChild(newIngredient);
+        deleteButton.addEventListener("click", () => {
+            ingredientsUL_2.removeChild(newIngredient);
+            // I need to work on removing the element from the list
+            ingredientsExclude = ingredientsExclude.filter(item => item != ingredient.textContent);
+            console.log(ingredientsExclude);
+        });
+        inputElmnt_2.focus();
+        inputElmnt_2.value = '';
+        console.log(ingredientsExclude);
+    } else if (keyPressed == "Enter" && inputElmnt_2.value == "") {
+        alert("Cannot add a blank ingredient. Add an ingredient and try again.");
+        inputElmnt_2.focus();
+    }
+});
+
+  formElmnt.appendChild(ingredients_exclude_H1);
+  formElmnt.appendChild(labelElmnt_2);
+  formElmnt.appendChild(inputElmnt_2);
+  formElmnt.appendChild(buttonElmnt_2);
+  formElmnt.appendChild(ingredientsUL_2);
 }
 
 function blankRecipe() {
