029a880cc0eae60a00cfa5f065f550b66819544d
jorted-runner <jorted.runner1996@gmail.com>
2024-02-18 18:17:59 -0600

    Start on recipe generation

--- Cook-Corner-Project/ai_interface.py
+++ Cook-Corner-Project/ai_interface.py
@@ -4,11 +4,12 @@ from openai import OpenAI
 import os
 
 load_dotenv()
-gpt_3_url = "https://api.openai.com/v1/chat/completions"
 
 class AI_tool():
   def __init__(self):
     self.client = OpenAI()
+    gpt_url = "https://api.openai.com/v1/chat/completions"
+
     
 
   def image_generation(self, prompt):
@@ -25,22 +26,26 @@ class AI_tool():
     return urls
     
 
-  # def recipe_generation(self, include, exclude):
-  #   prompt = f"Write a recipe with a descriptive name that includes {include} and any other needed ingredients but does not contain {exclude}. Output the Title, Description, Ingredients, and Instructions. Format the output using html tags, tagging the title with the <h2> tag." 
-  #   response = openai.ChatCompletion.create(
-  #     model="gpt-3.5-turbo",
-  #     messages=[
-  #       {"role": "user", "content": prompt}
-  #     ]
-  #   )
-  #   recipe = response.choices[0].message.content
-  #   # recipe_parts = recipe.split("Ingredients:")
-  #   # header = recipe_parts[0].strip()
-  #   # header_parts = header.split("Description:")
-  #   # title = header_parts[0].strip().split("Title:")[1]
-  #   # description = header_parts[1]
-  #   # ingredients = recipe_parts[1].split("Instructions:")[0].strip()
-  #   # instructions = recipe_parts[1].split("Instructions:")[1].strip()
-  #   # ingredients_list = ingredients.split("- ")
-  #   return recipe
+  def recipe_generation(self, include, exclude):
+    prompt = f"Write a recipe with a descriptive name that includes {include} and any other needed ingredients but does not contain {exclude}. Output the Title, Description, Ingredients, and Instructions. Format the output using html tags, tagging the title with the <h2> tag." 
+    response = self.client.chat.completions.create(
+      model="gpt-3.5-turbo-0125",
+      response_format={"type":"json_object"},
+      messages=[
+        {
+          {"role":"system","content":"You are a helpful assistant trained as a chef designed to output JSON."},
+          {"role": "user", "content": prompt}
+        }
+      ]
+    )
+    recipe = response.choices[0].message.content
+    # recipe_parts = recipe.split("Ingredients:")
+    # header = recipe_parts[0].strip()
+    # header_parts = header.split("Description:")
+    # title = header_parts[0].strip().split("Title:")[1]
+    # description = header_parts[1]
+    # ingredients = recipe_parts[1].split("Instructions:")[0].strip()
+    # instructions = recipe_parts[1].split("Instructions:")[1].strip()
+    # ingredients_list = ingredients.split("- ")
+    return recipe
     
