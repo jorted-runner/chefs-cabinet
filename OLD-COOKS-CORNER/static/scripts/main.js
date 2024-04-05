9389900671ca9db1838c2aed1982d25b2a6d0571
Danny Ellis <jorted.runner1996@gmail.com>
2024-04-05 11:21:00 -0500

    Delete OLD-COOKS-CORNER directory

--- OLD-COOKS-CORNER/static/scripts/main.js
+++
@@ -1,7 +0,0 @@
-let cardArray = document.querySelectorAll('.card__inner');
-
-cardArray.forEach(function(elem) {
-    elem.addEventListener('click', function() {
-        elem.classList.toggle('is-flipped');
-    });
-});
