d8c0e139bb8e09b5959ef1d8d67f76348e032605
jorted-runner <jorted.runner1996@gmail.com>
2024-04-27 21:14:10 -0500

    syntax fix

--- server.py
+++ server.py
@@ -166,8 +166,6 @@ def home():
     if current_user.is_authenticated:
         if current_user.following:
             following_ids = [follower.following_id for follower in current_user.following]
-            print(following_ids)
-
             all_recipes = Recipe.query.filter(or_(Recipe.user_id.in_(following_ids))).all()
         else:
             all_recipes = Recipe.query.all()
