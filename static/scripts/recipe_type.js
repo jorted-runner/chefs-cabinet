c4bd0cc7f88d82e7f0c3f64ce3848662351aee3c
jorted-runner <jorted.python@gmail.com>
2024-05-02 09:52:10 -0500

    recipe building form

--- static/scripts/recipe_type.js
+++ static/scripts/recipe_type.js
@@ -3,13 +3,6 @@ const loaderContainer = document.querySelector('.loader-container');
 const ai_form = document.querySelector('.ai-recipe');
 const blank_form = document.querySelector('.blank-recipe');
 
-const displayLoading = () => {
-    loaderContainer.style.display = 'block';
-};
-const hideLoading = () => {
-    loaderContainer.style.display = 'none';
-};
-
 var ingredientsInclude = [];
 var ingredientsExclude = [];
 var TITLE = '';
@@ -19,11 +12,30 @@ var INGREDIENTS = [];
 var IMAGE_URL = '';
 const saveRecipeURL = "/save_recipe";
 
+const displayLoading = () => {
+    loaderContainer.style.display = 'block';
+};
+const hideLoading = () => {
+    loaderContainer.style.display = 'none';
+};
+
+
 recipeTypeElmnt.addEventListener("change", function(event) {
     const selectedValue = event.target.value;
     if (selectedValue == "aiAssisted") {
         ai_form.classList.remove('hidden');
+        recipeTypeElmnt.disabled = true;
     } else if (selectedValue == "blankRecipe") {
         blank_form.classList.remove('hidden');
+        recipeTypeElmnt.disabled = true;
     }
 });
+
+function deleteEvent (event, ul, li, list) {
+    const eventText = event.target.parentNode.firstChild.textContent.trim();
+    ul.removeChild(li);
+    const indexToRemove = list.indexOf(eventText);
+    if (indexToRemove !== -1) {
+        list.splice(indexToRemove, 1);
+    }
+  }
